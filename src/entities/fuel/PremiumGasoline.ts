9514a80dabce57871da82ca104f31d01c487b517
vicmmont <vicmmonterroso@gmail.com>
2024-03-16 14:45:51 -0400

    adding eslint and script in package.json

--- src/entities/fuel/PremiumGasoline.ts
+++ src/entities/fuel/PremiumGasoline.ts
@@ -3,7 +3,10 @@ import { Fuel } from './Fuel';
 export class PremiumGasoline implements Fuel {
     private name = "Premium Gasoline";
 
+
     public combust: () => string = () => {
-        return `creating energy with ${this.name}`;
+        
+        
+                return `creating energy with ${this.name}`;
     };
 }
