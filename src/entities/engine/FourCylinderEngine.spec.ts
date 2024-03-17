209f3633a0875bfb4e65cb410b03433ba8163d3f
vicmmont <vicmmonterroso@gmail.com>
2024-03-16 22:52:37 -0400

    jest tests are operational

---
+++ src/entities/engine/FourCylinderEngine.spec.ts
@@ -0,0 +1,37 @@
+import "reflect-metadata";
+import { mock } from "jest-mock-extended";
+
+import { FourCylinderEngine } from "./FourCylinderEngine";
+import { Fuel } from "../fuel/Fuel";
+
+describe("four cylinder engine tests", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const mockFuel = mock<Fuel>();
+
+  it("combusts the fuel when the engine starts", () => {
+    const mockEngine = new FourCylinderEngine(mockFuel);
+
+    mockEngine.start();
+
+    expect(mockFuel.combust).toHaveBeenCalledTimes(1);
+  });
+
+  it("combusts the fuel when the engine runs", () => {
+    const mockEngine = new FourCylinderEngine(mockFuel);
+
+    mockEngine.run();
+
+    expect(mockFuel.combust).toHaveBeenCalledTimes(1);
+  });
+
+  it("does not combust the fuel when the engine shuts down", () => {
+    const mockEngine = new FourCylinderEngine(mockFuel);
+
+    mockEngine.shutDown();
+
+    expect(mockFuel.combust).not.toHaveBeenCalled();
+  });
+});
