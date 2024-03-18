2999d62d0211fbb157e062f80f42b2a122fa8a02
vicmmont <vicmmonterroso@gmail.com>
2024-03-17 21:48:24 -0400

    comment

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import TYPES from "./types";
 const container = setupContainer();
 const myCar: Vehicle = container.resolve(TYPES.Vehicle);
 
+// The car is going for a ride!
 console.log(myCar.start());
 console.log(myCar.drive());
 console.log(myCar.stop());
